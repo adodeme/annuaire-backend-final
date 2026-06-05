93c18c79f90e4099e493dd1d27e34b3e14f1deee
adodeme <adodemeflodias@gmail.com>
2026-06-05 15:36:06 +0100

    test without sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,27 @@
-Route::get('/me', [AuthController::class, 'me']);
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProfileController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/profiles', [ProfileController::class, 'index']);
+Route::get('/profiles/{id}', [ProfileController::class, 'show']);
+Route::get('/sectors', [ProfileController::class, 'sectors']);
+Route::get('/stats', [ProfileController::class, 'stats']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/me', [AuthController::class, 'me']);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/admin/profiles', [ProfileController::class, 'adminProfiles']);
+
+    Route::put('/admin/profiles/{id}/approve', [ProfileController::class, 'approve']);
+
+    Route::put('/admin/profiles/{id}/reject', [ProfileController::class, 'reject']);
+
+});
